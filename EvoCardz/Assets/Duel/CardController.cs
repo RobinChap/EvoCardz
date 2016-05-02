db84ae5b35833c0ca1d525eff96f5ae3b6a4072d
RobinChap <robinchap120@gmail.com>
2016-05-02 03:48:21 +0200

    First commit
    Added unity project

---
+++ EvoCardz/Assets/Duel/CardController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine;
+using System.Collections;
+
+public class CardController : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
